588d67541578633e8213c87fa0efe111c33fe8cf
Vinuthak <vinutha.kasaragod@gmail.com>
2024-11-08 22:46:39 +0100

    "First python project"

---
+++ first_python_app.py
@@ -0,0 +1,5 @@
+user_age = input("Enter your age")
+years = int(user_age)
+months = years * 12
+seconds = months * 30 * 24 * 60
+print(f"Your age is {years}, is equal to {months} months, is equal to {seconds} seconds.")
