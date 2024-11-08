22bac16780705d87063a896fd05d861695c4d0c2
Vinuthak <vinutha.kasaragod@gmail.com>
2024-11-08 22:33:37 +0100

    "Formatting_input and input"

---
+++ formattingstring.py
@@ -0,0 +1,6 @@
+name = "Bob"
+greeting = f"Hello, {name}"
+print(greeting)
+#template_format
+sum_template = "The sum of {0} and {1} is {4} and {3}"
+print(sum_template.format(10,26,(10+26),(26+10)))
---
+++ getting_user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+print(name)
+size_input = int(input("Enter a number:"))
+print(size_input)
