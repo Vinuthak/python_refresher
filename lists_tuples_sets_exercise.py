88ab246ecc61f2be6b5f0d8696e59ecbdb217c37
Vinuthak <vinutha.kasaragod@gmail.com>
2024-11-10 22:20:11 +0100

    "Simple exercises"

---
+++ lists_tuples_sets_exercise.py
@@ -0,0 +1,11 @@
+#Create a list, called my_list,with three numbers.The total of the numbers added together should be 100.
+my_list = [20,30,50]
+print(my_list)
+#Create a tuple,called my_tuple,with a single value in it.
+my_tuple = (4,)
+print(my_tuple)
+#Modify the variable set2 so that set1.intersection(set2) returns {5,77,9,12}
+set1 = {14,5,9,31,12,77,67,8}
+set2 = {5,6,77,10,9,12}
+intr = set1.intersection(set2) # => {5,77,9,12}
+print(intr)
