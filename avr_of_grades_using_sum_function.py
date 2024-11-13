42250353cd298819f2e27ae77e090eb12d22287a
Vinuthak <vinutha.kasaragod@gmail.com>
2024-11-13 22:09:29 +0100

    simple way to find sum instead of looping or using for loop

---
+++ avr_of_grades_using_sum_function.py
@@ -0,0 +1,6 @@
+grades = [35,67,98,100,100]
+num_of_grades = len(grades)
+sum_of_grades = sum(grades)
+print("Number of grades: ", num_of_grades)
+print("Sum of grades: ", sum_of_grades)
+print("The average is",sum_of_grades/num_of_grades)
