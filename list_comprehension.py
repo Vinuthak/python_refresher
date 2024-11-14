52d64cc0a322a5ee2c722b1a7d5f8ceb0c41f2be
Vinuthak <vinutha.kasaragod@gmail.com>
2024-11-14 14:24:11 +0100

    Added multiples of 5

--- list_comprehension.py
+++ list_comprehension.py
@@ -6,3 +6,6 @@ print(even_numbers)
 friends = ["Alex", "Romano","Sophia","Sarah","Star"]
 friends_s = [s for s in friends if s.startswith('S')]
 print(friends_s)
+
+multiples_of_5 = [x*5 for x in numbers]
+print(multiples_of_5)
